530a8e5df2dda6824958f8134c3a34f69c2e9a77
Richard Perry <richard@perrymail.me.uk>
2012-05-29 15:19:15 +0000

    Cleaned up header.php, removed unnecessary elements
    git-svn-id: https://perry-mnmlist.googlecode.com/svn/trunk@5 b3ab339a-6994-f9cd-0d2f-79844f2e9e21

--- header.php
+++ header.php
@@ -9,43 +9,27 @@
  * @since Perry Minimalist 1.0
  */
 ?><!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
-<meta name='author' content='Richard Perry' />
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title(''); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-<?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
-
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
-?>
+    <meta http-equiv="Content-Type" 
+        content="<?php bloginfo('html_type'); ?>; 
+        charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width" />
+    <title><?php wp_title(''); ?></title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <?php 
+    if ( is_singular() && get_option( 'thread_comments' ) ) 
+        wp_enqueue_script( 'comment-reply' );
+    
+    wp_head();
+    ?>
 </head>
 
 <body <?php body_class(); ?>>
